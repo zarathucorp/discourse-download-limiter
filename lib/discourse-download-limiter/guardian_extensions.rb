58a5cdc62de55dd570c9acb0669e401fa344a675
Changwoo Lim <limcw@zarathu.com>
2025-07-14 22:27:49 +0900

    로그 기능 추가

--- lib/discourse-download-limiter/guardian_extensions.rb
+++ lib/discourse-download-limiter/guardian_extensions.rb
@@ -1,23 +1,52 @@
 module DiscourseDownloadLimiter
   module GuardianExtension
     def can_download_upload?(upload)
-      # 1. Uploader can always download their own file
-      return true if user && user.id == upload.user_id
+      # --- 로그 시작 ---
+      Rails.logger.info "--- [Download Limiter Check] ---"
+      Rails.logger.info "File: #{upload.original_filename} (Upload ID: #{upload.id}), Uploader User ID: #{upload.user_id}"
 
-      # 2. Get the list of allowed group IDs from the setting
-      # The setting value is a string like "1|10|12"
+      # 요청한 유저 정보 확인
+      if user
+        Rails.logger.info "Requester: #{user.username} (User ID: #{user.id})"
+        Rails.logger.info "Requester's Group IDs: #{user.group_ids.inspect}"
+      else
+        Rails.logger.info "Requester: Not logged in."
+      end
+      # --- 로그 준비 완료 ---
+
+      # 1. 업로더 본인인지 확인
+      if user && user.id == upload.user_id
+        Rails.logger.info "Decision: ALLOWED (User is the uploader)."
+        Rails.logger.info "------------------------------------"
+        return true
+      end
+
+      # 2. 설정에서 허용된 그룹 목록 가져오기
       allowed_group_ids_str = SiteSetting.download_limiter_allowed_groups
+      Rails.logger.info "Allowed Group IDs from Setting: #{allowed_group_ids_str.inspect}"
 
-      # If no group is set, allow everyone
-      return true if allowed_group_ids_str.blank?
+      # 3. 설정된 그룹이 없으면 모두 허용
+      if allowed_group_ids_str.blank?
+        Rails.logger.info "Decision: ALLOWED (No group restriction is set)."
+        Rails.logger.info "------------------------------------"
+        return true
+      end
 
-      # 3. Convert the string of IDs into an array of integers
+      # 4. 사용자가 허용된 그룹에 속해 있는지 확인
       allowed_group_ids = allowed_group_ids_str.split('|').map(&:to_i)
+      
+      # 로그인을 안 했거나 그룹이 없으면 is_member는 false가 됨
+      is_member = user && (user.group_ids & allowed_group_ids).any?
 
-      # 4. Check if the user is a member of ANY of the allowed groups
-      # .pluck(:id) gets all group IDs for the current user
-      # `&` is the array intersection operator. If there's any overlap, the result won't be empty.
-      user && (user.group_ids & allowed_group_ids).any?
+      if is_member
+        Rails.logger.info "Decision: ALLOWED (User is in an allowed group)."
+        Rails.logger.info "------------------------------------"
+        return true
+      else
+        Rails.logger.info "Decision: DENIED (User is not uploader and not in an allowed group)."
+        Rails.logger.info "------------------------------------"
+        return false
+      end
     end
   end
 end
