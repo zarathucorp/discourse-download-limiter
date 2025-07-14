2b7d1955233711053a0b90fe9f9d8c36e676b244
Changwoo Lim <limcw@zarathu.com>
2025-07-14 23:06:30 +0900

    short url일때의 로직 수정

--- plugin.rb
+++ plugin.rb
@@ -15,21 +15,19 @@ after_initialize do
     private
 
     def check_download_permission
-      # ✅ 'show'와 'show_short' 경로에 따라 파일을 찾는 로직을 수정
       upload = nil
       if params[:id].present?
         upload = Upload.find_by(id: params[:id])
       elsif params[:base62].present?
-        upload = Upload.find_by(short_url: params[:base62])
+        # ✅ 이 부분이 최종 수정되었습니다.
+        upload = Upload.find_by_short_url(params[:base62])
       end
 
-      # Get the requested upload
-      return if upload.nil? # Should be handled by Discourse, but as a safeguard
+      return if upload.nil?
 
-      return if guardian.is_admin? # Admins can always download
+      return if guardian.is_admin?
 
       unless guardian.can_download_upload?(upload)
-        # If the user doesn't have permission, deny access
         raise Discourse::InvalidAccess.new("You do not have permission to download this file.")
       end
     end
