1c1e22ce6eae8beeaa23dc5997d525e9a2bf0222
Changwoo Lim <limcw@zarathu.com>
2025-07-14 22:57:45 +0900

    baae62 대응

--- plugin.rb
+++ plugin.rb
@@ -6,23 +6,27 @@
 
 enabled_site_setting :download_limiter_enabled
 
-# Load the guardian extension
-require_relative 'lib/discourse-download-limiter/guardian_extensions'
+require_relative 'lib/discourse-download-limiter/guardian_extension'
 
 after_initialize do
-  # Prepend our custom module to the UploadsController
-  # This is the standard way to add logic before the original method runs
   UploadsController.class_eval do
     prepend_before_action :check_download_permission, only: [:show, :show_short]
 
     private
 
     def check_download_permission
-      return if guardian.is_admin? # Admins can always download
+      # ✅ 'show'와 'show_short' 경로에 따라 파일을 찾는 로직을 수정
+      upload = nil
+      if params[:id].present?
+        upload = Upload.find_by(id: params[:id])
+      elsif params[:base62].present?
+        upload = Upload.find_by(short_url: params[:base62])
+      end
 
       # Get the requested upload
-      upload = Upload.find_by(id: params[:id])
       return if upload.nil? # Should be handled by Discourse, but as a safeguard
+
+      return if guardian.is_admin? # Admins can always download
 
       unless guardian.can_download_upload?(upload)
         # If the user doesn't have permission, deny access
@@ -31,7 +35,5 @@ after_initialize do
     end
   end
 
-  # Extend the Guardian with our custom logic
-  # The Guardian is Discourse's central permission checking class
   Guardian.class_eval { include DiscourseDownloadLimiter::GuardianExtension }
 end
